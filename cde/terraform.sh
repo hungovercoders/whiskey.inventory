4f23633efedb0f4e749a089d64d64aa1ec684539
griff182uk <griff182uk@yahoo.co.uk>
2024-01-21 14:34:29 +0000

    Refactor environment variables and backend configuration

--- cde/terraform.sh
+++ cde/terraform.sh
@@ -3,15 +3,26 @@ echo "Apply: $apply"
 set -a
 . ./domain.env
 set +a
-TF_BACKEND_RESOURCE_GROUP="state-rg-$UNIQUE_NAMESPACE"
-TF_BACKEND_STORAGE_ACCOUNT="statesa$UNIQUE_NAMESPACE"
-TF_BACKEND_CONTAINER=$ENVIRONMENT
+
 export TF_VAR_environment=$ENVIRONMENT
 export TF_VAR_unique_namespace=$UNIQUE_NAMESPACE
 export TF_VAR_organisation=$ORGANISATION
 export TF_VAR_region=$ARM_REGION
 export TF_VAR_team=$TEAM
 export TF_VAR_domain=$DOMAIN
+export TF_BACKEND_CONTAINER=$ENVIRONMENT
+TF_BACKEND_RESOURCE_GROUP="state-rg-$UNIQUE_NAMESPACE"
+TF_BACKEND_STORAGE_ACCOUNT="statesa$UNIQUE_NAMESPACE"
+
+echo "Unique Namespace is $TF_VAR_unique_namespace" 
+echo "Organisation is $TF_VAR_organisation" 
+echo "Region is $TF_VAR_region" 
+echo "Environment is $ENVIRONMENT" 
+echo "Team is $TF_VAR_team" 
+echo "Domain is $TF_VAR_domain" 
+echo "State Storage Account Resource Group is $TF_BACKEND_RESOURCE_GROUP" 
+echo "State Storage Account is $TF_BACKEND_STORAGE_ACCOUNT" 
+echo "State Storage Account Container is $TF_BACKEND_CONTAINER"
 
 cd  terraform
 terraform init -backend-config="resource_group_name=$TF_BACKEND_RESOURCE_GROUP" -backend-config="storage_account_name=$TF_BACKEND_STORAGE_ACCOUNT" -backend-config="container_name=$TF_BACKEND_CONTAINER"
--- cde/variables.sh
+++ cde/variables.sh
@@ -1,6 +1,23 @@
+set -a
+. ./domain.env
+set +a
+
+export TF_VAR_environment=$ENVIRONMENT
+export TF_VAR_unique_namespace=$UNIQUE_NAMESPACE
+export TF_VAR_organisation=$ORGANISATION
+export TF_VAR_region=$ARM_REGION
+export TF_VAR_team=$TEAM
+export TF_VAR_domain=$DOMAIN
+export TF_BACKEND_CONTAINER=$ENVIRONMENT
+TF_BACKEND_RESOURCE_GROUP="state-rg-$UNIQUE_NAMESPACE"
+TF_BACKEND_STORAGE_ACCOUNT="statesa$UNIQUE_NAMESPACE"
+
 echo "Unique Namespace is $TF_VAR_unique_namespace" 
 echo "Organisation is $TF_VAR_organisation" 
 echo "Region is $TF_VAR_region" 
-echo "Storage Account is $TF_BACKEND_STORAGE_ACCOUNT" 
 echo "Environment is $ENVIRONMENT" 
-echo "Container is $TF_BACKEND_CONTAINER"
+echo "Team is $TF_VAR_team" 
+echo "Domain is $TF_VAR_domain" 
+echo "State Storage Account Resource Group is $TF_BACKEND_RESOURCE_GROUP" 
+echo "State Storage Account is $TF_BACKEND_STORAGE_ACCOUNT" 
+echo "State Storage Account Container is $TF_BACKEND_CONTAINER"
