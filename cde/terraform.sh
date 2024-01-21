73bda470d4342af8ea16eee5e4e3f1689fdbf788
griff182uk <griff182uk@yahoo.co.uk>
2024-01-21 15:04:48 +0000

    Refactor Terraform workflow and scripts

--- cde/terraform.sh
+++ cde/terraform.sh
@@ -4,6 +4,8 @@ set -a
 . ./domain.env
 set +a
 
+echo "MESSAGE: Setting terraform state variables..."
+
 export TF_VAR_environment=$ENVIRONMENT
 export TF_VAR_unique_namespace=$UNIQUE_NAMESPACE
 export TF_VAR_organisation=$ORGANISATION
@@ -13,6 +15,8 @@ export TF_VAR_domain=$DOMAIN
 export TF_BACKEND_CONTAINER=$ENVIRONMENT
 TF_BACKEND_RESOURCE_GROUP="state-rg-$UNIQUE_NAMESPACE"
 TF_BACKEND_STORAGE_ACCOUNT="statesa$UNIQUE_NAMESPACE"
+
+echo "MESSAGE: Terraform state variables are..."
 
 echo "Unique Namespace is $TF_VAR_unique_namespace" 
 echo "Organisation is $TF_VAR_organisation" 
@@ -24,12 +28,22 @@ echo "State Storage Account Resource Group is $TF_BACKEND_RESOURCE_GROUP"
 echo "State Storage Account is $TF_BACKEND_STORAGE_ACCOUNT" 
 echo "State Storage Account Container is $TF_BACKEND_CONTAINER"
 
+echo "MESSAGE: Changing to terraform directory..."
 cd  terraform
+
+echo "MESSAGE: Initalising terraform..."
 terraform init -backend-config="resource_group_name=$TF_BACKEND_RESOURCE_GROUP" -backend-config="storage_account_name=$TF_BACKEND_STORAGE_ACCOUNT" -backend-config="container_name=$TF_BACKEND_CONTAINER"
+
+echo "MESSAGE: Formatting terraform..."
 terraform fmt
+
+echo "MESSAGE: Validating terraform..."
 terraform validate
+
+echo "MESSAGE: Planning terraform..."
 terraform plan
+
 if [ $apply = True ]; then
-    echo "Applying..."
+    echo "MESSAGE: Applying terraform..."
     terraform apply -auto-approve
 fi
