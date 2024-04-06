b2d0fdada00fba4cd5b2c5646ba03dc109955632
griff182uk <griff182uk@yahoo.co.uk>
2024-04-06 15:08:05 +0000

    page works locally

--- api/api/Program.cs
+++ api/api/Program.cs
@@ -4,6 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:8080") // Corrected URL
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -13,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowSpecificOrigins");
 
 app.UseHttpsRedirection();
 
--- tools_app/preview.sh
+++ tools_app/preview.sh
@@ -2,8 +2,10 @@ set -a
 . ./domain.env
 set +a
 echo -e "${MESSAGE_COLOUR}Starting script: $0...${MESSAGE_NO_COLOUR}"
+url="http://localhost:$PORT/distilleries"
+echo -e "Opening $url in a web browser..."
 set -a
 . ./domain.env
 set +a
-python3 -m webbrowser http://localhost:$PORT/distilleries
+python3 -m webbrowser $url
 echo -e "${MESSAGE_COLOUR}Completed script: $0.${MESSAGE_NO_COLOUR}"
